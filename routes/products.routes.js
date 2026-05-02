567e023d6603489b590753fab6ecb3be65b7afeb
Valentina Molina <valemolina226@gmail.com>
2026-05-02 15:23:42 -0300

    Actualizo products.routes.js

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -11,8 +11,31 @@ router.get('/', async (req, res) => {
 
 /* PUT */
 router.put('/:id', async (req, res) => {
-  const updated = await updateProduct(req.params.id, req.body)
-  res.json(updated)
+  try {
+  
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ error: 'Faltan datos para actualizar' })
+    }
+
+
+    const allowedFields = ['nombre', 'desc', 'precio', 'imagen', 'stock']
+    const bodyKeys = Object.keys(req.body)
+
+
+    const isValidOperation = bodyKeys.every(key => allowedFields.includes(key))
+
+    if (!isValidOperation) {
+      return res.status(400).json({ 
+        error: 'Estás intentando ingresar datos no válidos. Solo se permite actualizar: nombre, desc, precio, imagen y stock.' 
+      })
+    }
+
+
+    const updated = await updateProduct(req.params.id, req.body)
+    res.json(updated)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
 })
 
 export default router
